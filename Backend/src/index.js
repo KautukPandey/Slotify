5430a49d2c1a127108c20633398673ea070b592b
Kautuk <kautukpandey2004@gmail.com>
2026-04-23 16:48:33 +0530

    Auth working

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,15 +2,16 @@ import express from "express"
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import { connectDB } from "./db/db.js"
-
+import dotenv from "dotenv"
+dotenv.config()
 connectDB()
 const app = express()
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
-
+console.log("ENV:", process.env.MONGODB_URI)
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`);
     
